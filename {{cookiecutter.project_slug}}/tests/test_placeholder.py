d7b21f70f851d83dea18fdc4903b19ece8ecf68e
juwagner <juwagner1989@gmail.com>
2026-05-04 13:05:49 +0200

    Add test placeholder

---
+++ {{cookiecutter.project_slug}}/tests/test_placeholder.py
@@ -0,0 +1,10 @@
+"""Test placeholder."""
+
+
+def test_placeholder() -> None:
+    """
+    Test placeholder test to prevent CI from failing.
+
+    Remove once real tests are added to the project.
+    """
+    assert True
